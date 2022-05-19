35be0a9c2e545854701b0c388a13b6c8c7418b00
Johan Sebastiampillai <johan15_seb@hotmail.com>
2022-05-18 22:55:19 -0400

    Update scripts

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 // Middleware
 app.use(express.json()); // parse json bodies in the request object
